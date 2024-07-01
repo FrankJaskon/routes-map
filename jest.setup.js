f4452845d85cd30a62b308f0daedea20216a863c
frankjaskon <frankjaskon@gmail.com>
2024-07-01 16:46:14 +0300

    chore: update credentials

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
 require('@testing-library/jest-dom')
 
-process.env.API_KEY = 'AIzaSyAfEjWsUuObNl3xsbONzftKIbeM6h3yDbs'
+process.env.API_KEY = 'AIzaSyDa-V8W8Aalo6ggYLW2gE3VJr68mqvuf_M'
 process.env.MAP_ID = 'b00b00e77698909c'
